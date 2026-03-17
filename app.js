6ea110844773cadefbecb8ce3a746b53bbf14868
rakesh <rakeshravichandran001@gmail.com>
2026-03-17 09:03:10 +0530

    second changes

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-const parent = [
-  React.createElement("h1", { id: "heading" }, "Hello World!"),
-  React.createElement(
-    "a",
-    { href: "https://www.youtube.com/", target: "_blank" },
-    "Youtube",
-  ),
-];
-const divId = ReactDOM.createRoot(document.querySelector("#root"));
-//divId.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const jsxh = <h1>React course</h1>;
 
-const divChild = [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading" }, "Hello World!"),
-    React.createElement(
-      "a",
-      { href: "https://www.youtube.com/", target: "_blank" },
-      "Youtube",
-    ),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h2", { id: "heading" }, "Hello World!"),
-    React.createElement(
-      "a",
-      { href: "https://www.youtube.com/", target: "_blank" },
-      "Youtube",
-    ),
-  ]),
-];
-divId.render(divChild);
+const Comp = () => (
+  <div>
+    {jsxh}
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIsWRcoz1h2eUxA3KtKJhrsEtbDtN60dwJdg&s"
+      alt="Harry Potter"
+    />
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Comp />);
